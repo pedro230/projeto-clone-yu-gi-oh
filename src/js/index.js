d7388e69606bb75f9668637634f416ccb08e77e9
Pedro Ladeira <111195104+pedro230@users.noreply.github.com>
2025-01-26 22:24:23 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,23 @@ const btnAvancar = document.getElementById("btn-avancar");
 const cartoes = document.querySelectorAll(".cartao")
 let cartaoAtual = 0;
 
+cartoes.forEach(cartao => {
+    cartao.addEventListener("click", function () {
+        const cartaVirada = cartao.querySelector(".carta-virada");
+        cartao.classList.toggle("virar");
+        cartaVirada.classList.toggle("mostrar-fundo-carta");
+
+        const descricao = cartao.querySelector(".descricao");
+        descricao.classList.toggle("esconder")
+})
+});
+
 btnAvancar.addEventListener("click", function () {
     console.log(cartaoAtual);
-    console.log(cartoes.length - 1) 
+    console.log(cartoes.length - 1)
     if (cartaoAtual === cartoes.length - 1) return;
 
-    
+
     const cartaoSelecionado = document.querySelector(".selecionado")
     cartaoSelecionado.classList.remove("selecionado");
 
@@ -15,7 +26,7 @@ btnAvancar.addEventListener("click", function () {
     cartoes[cartaoAtual].classList.add("selecionado");
 });
 
-const btnVoltar= document.getElementById("btn-voltar");
+const btnVoltar = document.getElementById("btn-voltar");
 
 btnVoltar.addEventListener("click", function () {
 
